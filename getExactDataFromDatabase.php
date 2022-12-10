d113d0569b9995fd750f7033040358cf813d7dd5
Gökhan Çava <gokhancava@gmail.com>
2022-12-10 23:13:07 +0300

    Create getExactDataFromDatabase.php
    In this code as you see, We fetch data from database and wait for 5 seconds and get data again, if both datas are same, it is okey. But if they are different, it should fetch again.
    This is a method, you can develop it and use it.

---
+++ getExactDataFromDatabase.php
@@ -0,0 +1,72 @@
+<?php
+function loadDataBefore($limit,$db2,$minimum){
+  global $baglan;
+  global $db;
+
+
+
+for(;;){
+    $beforeAlgo2 = [];
+    $timeCekilenArr = [];
+    $yuzdeArr = [];
+    $timeArrReal = [];
+
+      $query2 = $baglan->query("SELECT * From $db2 where coin='$db' and profit>=$minimum order by id desc limit $limit");
+      while($veri2=$query2->fetch_array()) {
+
+
+        $beforeAlgo2[] = $veri2[1];
+        $timeCekilenArr[] = $veri2[5];
+        $yuzdeArr[] = $veri2[3];
+        $timeArrReal[] = $veri2[7];
+
+      }
+
+if(count($beforeAlgo2)=="0")
+sleep(10);
+else
+break;
+}
+
+return $beforeAlgo2;
+}
+
+function loadData($limit,$db2,$minimum){
+  global $baglan;
+  global $db;
+
+
+for(;;){
+    $beforeAlgo2 = [];
+    $timeCekilenArr = [];
+    $yuzdeArr = [];
+    $timeArrReal = [];
+
+$beforeBeforeAlgo2 = loadDataBefore($limit,$db2,$minimum);
+sleep(5);
+
+      $query2 = $baglan->query("SELECT * From $db2 where coin='$db' and profit>=$minimum order by id desc limit $limit");
+      while($veri2=$query2->fetch_array()) {
+
+
+        $beforeAlgo2[] = $veri2[1];
+        $timeCekilenArr[] = $veri2[5];
+        $yuzdeArr[] = $veri2[3];
+        $timeArrReal[] = $veri2[7];
+
+      }
+
+      if($beforeBeforeAlgo2!=$beforeAlgo2)
+      continue;
+
+
+if(count($beforeAlgo2)=="0")
+sleep(10);
+else
+break;
+}
+
+
+
+
+}
